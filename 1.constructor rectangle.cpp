304388c9c930451f27a5f62a2cf89627e9275aa5
Rushi-Shirke <110587748+Rushi-Shirke@users.noreply.github.com>
2022-08-04 11:49:03 +0530

    first commit.

---
+++ 1.constructor rectangle.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class rectangle
+{
+	private:
+		int len,bre;
+	public:
+		rectangle();
+		rectangle(int l,int b);
+		int rectarea();
+};
+rectangle::rectangle()
+{
+	len=4;
+	bre=5;
+}
+rectangle::rectangle(int l,int b)
+{
+	len=l;
+	bre=b;
+}
+int rectangle::rectarea()
+{
+	return len*bre;
+}
+main()
+{
+	rectangle r1;
+	cout<<"\n Area of rectangle="<<r1.rectarea();
+	
+	rectangle r2(9,8);
+	cout<<"\n Area of rectangle="<<r2.rectarea();
+}
